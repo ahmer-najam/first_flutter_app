4b25f16254af3c2d2ae97e6c6464f5b0917b1e3c
ahmer-najam <properpakistani@gmail.com>
2020-12-30 07:08:49 +0500

    2020.12.30.001

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      if(_questionIndex == 0){
+        _questionIndex++;          
+      }
+    });
+    print('Answer chosen!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite color?',
+      'What is your favourite car?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Aham'),
         ),
-        body: Column(children: [
-          Text('What\'s your favourite color?'),
-          RaisedButton(child: Text('Red'), onPressed: null,),
-          RaisedButton(child: Text('Blue'), onPressed: null,),
-          RaisedButton(child: Text('Black'), onPressed: null,),
-        ],),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Red'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Blue'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Black'),
+              onPressed: () {
+                print('Answer3 chosen..');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
